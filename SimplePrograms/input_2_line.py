8734e5e01c8ad17d1ba90ac7e5a4d9baf3a3ab02
Mark Ran <ranjun@wondershare.com>
2017-06-30 16:15:56 +0800

    practice for if

--- SimplePrograms/input_2_line.py
+++ SimplePrograms/input_2_line.py
@@ -1,2 +1,12 @@
 name = raw_input('What is your name?\n')
 print 'Hi, %s.' % name
+
+#My practice
+age = raw_input('How old are your?\n')
+age_int = int(age)
+if age_int > 20 and age_int <=40:
+    print 'Hi, %s, your are young' %name
+elif age_int > 40 and age_int <= 80:
+    print 'Hi, %s, o.o' %name
+elif age_int > 80:
+    print 'Hi, %s, your are older man' %name
